c550d271435be75e5c2be9b45894968b8092b769
Ahmet Okay <ahmet.okay@outlook.com>
2017-10-12 10:16:03 +0300

    Initial commit.

--- core-util/src/main/java/com/okay/DataUtil.java
+++ core-util/src/main/java/com/okay/DataUtil.java
@@ -1,4 +1,5 @@
 package com.okay;
 
 public class DataUtil {
+
 }
---
+++ core-util/src/main/java/com/okay/DateUtil.java
@@ -0,0 +1,22 @@
+package com.okay;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+
+    public static String dateToString(Date date, String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(date);
+    }
+
+    public static Date stringToDate(String date, String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        try {
+            return sdf.parse(date);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+}
